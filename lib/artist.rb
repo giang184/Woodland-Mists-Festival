298c1801999eb403df6deaed32f62e39e5bb7d85
Andrew Giang <giang184@gmail.com>
2021-07-21 14:38:16 -0700

    add artist class
    Co-authored-by: Dave Lindqvist <Lindqvist.Dave@gmail.com>
    Co-Authored-By: Kyle Williamson <kylewilliamsonv1@gmail.com>

---
+++ lib/artist.rb
@@ -0,0 +1,57 @@
+class Artist
+  attr_reader :id
+  attr_accessor :name, :stage_id
+
+  @@artists = {}
+  @@total_rows = 0
+
+  def initialize(name, stage_id, id)
+    @name = name
+    @stage_id = stage_id
+    @id = id || @@total_rows += 1
+  end
+
+  def ==(artist_to_compare)
+    (self.name() == artist_to_compare.name()) && (self.album_id() == artist_to_compare.album_id())
+  end
+
+  def self.all
+    @@artists.values
+  end
+
+  def save
+    @@artists[self.id] = Artist.new(self.name, self.album_id, self.id)
+  end
+
+  def self.find(id)
+    @@artists[id]
+  end
+
+  def update(name, album_id)
+    self.name = name
+    self.album_id = album_id
+    @@artists[self.id] = Artist.new(self.name, self.album_id, self.id)
+  end
+
+  def delete
+    @@artists.delete(self.id)
+  end
+
+  def self.clear
+    @@artists = {}
+  end
+
+  def self.find_by_stage(stg_id)
+    artists = []
+    @@artists.values.each do |artist|
+      if artist.stage_id == stg_id
+        artists.push(artist)
+      end
+    end
+    artists
+  end
+
+  def stage
+    stage.find(self.stage_id)
+  end
+end
--- spec/stage_spec.rb
+++ spec/stage_spec.rb
@@ -3,6 +3,10 @@ require 'stage'
 
 describe '#Stage' do
 
+  before(:each) do
+    Stage.clear()
+  end
+
   describe('.all') do
     it("returns an empty array when there are no stages") do
       expect(Stage.all ).to(eq([]))
@@ -31,6 +35,36 @@ describe '#Stage' do
     stage1.save()
     Stage.clear()
     expect(Stage.all).to(eq([]))
+    end
+  end
+  
+  describe('.find') do
+    it("finds an stage by id") do
+      stage1 = Stage.new("Ga-Zoinks", nil)
+      stage1.save()
+      stage2 = Stage.new("Clam Chowder", nil)
+      stage2.save()
+      expect(Stage.find(stage1.id)).to(eq(stage1))
+    end
+  end
+
+  describe('#update') do
+    it("updates an album by id") do
+      stage1 = Stage.new("Ga-Zoinks", nil)
+      stage1.save()
+      stage1.update("Love Jam")
+      expect(stage1.name).to(eq("Love Jam"))
+    end
+  end
+  
+  describe('#delete') do
+    it("deletes an album by id") do
+      stage1 = Stage.new("Ga-Zoinks", nil)
+      stage1.save()
+      stage2 = Stage.new("Clam Chowder", nil)
+      stage2.save()
+      stage1.delete()
+      expect(Stage.all).to(eq([stage2]))
     end
   end
 end
